ef85996c29f4fca34fa63aea771c4a0c18a33840
John <johnopana2016@gmail.com>
2019-12-07 00:06:35 +0300

    added constructure and the user name

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { Http,Headers } from  '@angular/http';
+import 'rxjs/add/operator/map';
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  private clientid ='';
+  private clientsecret ='';
+
+  constructor(private http:Http) { 
+    console.log("service is ready"!);
+    this.username ="Opana";
+  }
+  getprofileInfo(){
+    return this.http.get()
+
+  }
+  
 }
